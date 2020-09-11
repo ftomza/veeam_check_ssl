357543e7c3196c7affebe3c4fbb0dea0862fdc2b
Artem V. Zaborskiy <ftomza@yandex.ru>
2020-09-11 18:01:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio
+
+
+async def main():
+    with open("input.txt", "r") as f:
+        for i in f.readline():
+                
+    queue = asyncio.Queue()
+    for i in []
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
